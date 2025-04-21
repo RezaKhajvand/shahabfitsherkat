3e5906edbd2bff4e904833a23c62333aa8a87d16
Reza Khajvand <rezakhajvand77@gmail.com>
2025-04-21 11:34:44 +0330

    finger

--- lib/Constants/pb.dart
+++ lib/Constants/pb.dart
@@ -3,4 +3,4 @@ import 'package:pocketbase/pocketbase.dart';
 // http://127.0.0.1:8090/
 // https://bermooda.liara.run
 
-var pb = PocketBase('http://127.0.0.1:8090');
+var pb = PocketBase('https://bermooda.liara.run');
