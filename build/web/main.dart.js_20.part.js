861031830d123b1b2197b24031b99ada63347ca7
Reza Khajvand <rezakhajvand77@gmail.com>
2025-07-13 17:06:04 +0330

    26

--- build/web/main.dart.js_20.part.js
+++
@@ -1,9 +0,0 @@
-((a,b,c)=>{a[b]=a[b]||{}
-a[b][c]=a[b][c]||[]
-a[b][c].push({p:"main.dart.js_20",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[0]
-B=c[27]
-var z=a.updateTypes([]);(function constants(){B.Pp=new A.dh("\u0627\u0641\u0632\u0648\u062f\u0646",null,null,null,null,null,null,null,null,null,null)})()};
-((a,b)=>{a[b]=a.current
-a.eventLog.push({p:"main.dart.js_20",e:"endPart",h:b})})($__dart_deferred_initializers__,"2/3w21/i0nIPYKSl1PemBGm9wkk=");
--- build/web/main.dart.js_21.part.js
+++
@@ -1,9 +0,0 @@
-((a,b,c)=>{a[b]=a[b]||{}
-a[b][c]=a[b][c]||[]
-a[b][c].push({p:"main.dart.js_21",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[0]
-B=c[28]
-var z=a.updateTypes([]);(function constants(){B.Om=new A.d1("\u0627\u0641\u0632\u0648\u062f\u0646",null,null,null,null,null,null,null,null,null,null)})()};
-((a,b)=>{a[b]=a.current
-a.eventLog.push({p:"main.dart.js_21",e:"endPart",h:b})})($__dart_deferred_initializers__,"GdTHlD6WUxj1V09CCCpbpyJS4oE=");
