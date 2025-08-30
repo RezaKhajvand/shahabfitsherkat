e1481d8497cd63cc5acf7d4aaf44af9662baaf40
Reza Khajvand <rezakhajvand77@gmail.com>
2025-08-30 13:19:27 +0330

    1.1.22

--- lib/Features/Basket/Data/addDescriptionDataSource.dart
+++ lib/Features/Basket/Data/addDescriptionDataSource.dart
@@ -1,8 +1,13 @@
 import 'package:shahabfit/Features/Basket/Utils/descriptiontype.dart';
+import 'package:shahabfit/Utils/authmanager.dart';
 import 'package:shahabfit/constants/pb.dart';
 
 Future<String> addDescription(String text, DescriptionType type) async {
-  var body = {"text": text, "type": getDescriptionTitle(type)};
+  var body = {
+    "text": text,
+    "type": getDescriptionTitle(type),
+    "user": AuthManager.readUser(),
+  };
   try {
     final record = await pb.collection('description').create(body: body);
     print(record.toString());
